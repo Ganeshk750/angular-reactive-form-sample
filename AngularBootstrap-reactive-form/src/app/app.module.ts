4b9fcd934d5bcef44cefb1b93bd486af91e34e36
Ganesh Kumar <47558133+Ganeshk750@users.noreply.github.com>
2021-09-09 11:55:05 +0530

    # adding routing module adding required module in appMoule

--- AngularBootstrap-reactive-form/src/app/app.module.ts
+++ AngularBootstrap-reactive-form/src/app/app.module.ts
@@ -1,14 +1,73 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//import { MatExpansionModule } from '@angular/material/expansion';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NotifierModule, NotifierOptions } from 'angular-notifier';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { ModalModule } from 'ngb-modal';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+const customNotifierOptions: NotifierOptions = {
+  position: {
+    horizontal: {
+      position: 'left',
+      distance: 12
+    },
+    vertical: {
+      position: 'bottom',
+      distance: 12,
+      gap: 10
+    }
+  },
+  theme: 'material',
+  behaviour: {
+    autoHide: 5000,
+    onClick: 'hide',
+    onMouseover: 'pauseAutoHide',
+    showDismissButton: true,
+    stacking: 4
+  },
+  animations: {
+    enabled: true,
+    show: {
+      preset: 'slide',
+      speed: 300,
+      easing: 'ease'
+    },
+    hide: {
+      preset: 'fade',
+      speed: 300,
+      easing: 'ease',
+      offset: 50
+    },
+    shift: {
+      speed: 300,
+      easing: 'ease'
+    },
+    overlap: 150
+  }
+};
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    //MatExpansionModule,
+    BrowserAnimationsModule,
+    NotifierModule.withConfig(customNotifierOptions),
+    ModalModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
